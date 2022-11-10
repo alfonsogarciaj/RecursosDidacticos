763d95fdda4dd3e06736380bb9cc1bd374f6c915
Alfonso García Jorge <alfonsogjorge@gmail.com>
2022-11-10 13:16:45 +0000

    Update main.py

--- Pyhton/main.py
+++ Pyhton/main.py
@@ -1,3 +1,67 @@
-# This is a sample Python script.
+"""
+Pyhton:
 
+Creado por Guido Van Rossum a los 90.
+Lenguaje de alto nivel. Tipado dinamico y fuerte.
+Orientados a objetos:
+    Sobrecarga de constructores. Herencia multiple.Encapsulacion. Interfaces. Polimorfismo
+Librería estandar muy amplia
+Interpretado
+Versatil
+>>> Prompt
+"""
 
+# Imprimir
+print("Hola Mundo")
+
+# \ para dividir
+miNombre= "Hola soy Alfonso"
+print(miNombre)
+miNombre= "Hola soy\
+          Alfonso"
+
+#Identacion
+#Tabulacion para identar en bucles
+a = 0
+for i in range(5):
+    a+=1
+    print(a)
+
+"""
+Tipos: numericos textos y booleanos
+Los numericos: int float y complejos
+Booleanos: True y False
+"""
+
+"""
+Operadores:
++ - * / % 
+exponente ** 
+division entera//
+
+Comparaciones:
+==
+!=
+>
+<
+>=
+<=
+
+Logicos:
+AND OR NOT
+
+Asignacion
+=
++=
+-=
+*=
+%=
+**=
+//=
+
+Especiales:
+IS 
+IS NOT 
+IN 
+NOT IN
+"""
